3ab96cf6c6086e1729a47832e3a979aef5949150
Khairo Khatib <khairo.kh@outlook.com>
2021-06-11 01:16:17 -0400

    #9 set up initial configuration for video calls

--- client/src/pages/sessionPage/session.js
+++ client/src/pages/sessionPage/session.js
@@ -1,12 +1,104 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Peer from "simple-peer";
+import io from "socket.io-client";
+
 import "../../css/Editor.css";
 import firebase from "firebase";
 import Result from "./result";
 
+const socket = io.connect("http://localhost:8000");
+
 const Editor = () => {
   const [data, setData] = useState("");
   const [padRef, setPadRef] = useState("");
+  const [self, setSelf] = useState("");
+  const [stream, setStream] = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState("");
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [idToCall, setIdToCall] = useState("");
+  const [callEnded, setCallEnded] = useState(false);
+  const [name, setName] = useState("");
+
+  const myVid = useRef();
+  const peerVid = useRef();
+  const callRef = useRef();
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        setStream(stream);
+        myVid.current.srcObject = stream;
+      });
+
+    socket.on("me", (id) => {
+      setSelf(id);
+    });
+
+    socket.on("callUser", (data) => {
+      setReceivingCall(true);
+      setCaller(data.from);
+      setName(data.name);
+      setCallerSignal(data.signal);
+    });
+  }, []);
+
+  const callUser = (id) => {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream: stream,
+    });
+
+    peer.on("signal", (data) => {
+      socket.emit("callUser", {
+        userToCall: id,
+        signalData: data,
+        from: self,
+        name: name,
+      });
+    });
+
+    peer.on("stream", (stream) => {
+      peerVid.current.srcObject = stream;
+    });
+
+    socket.on("callAccepted", (signal) => {
+      setCallAccepted(true);
+      peer.signal(signal);
+    });
+
+    callRef.current = peer;
+  };
+
+  const answerCall = () => {
+    setCallAccepted(true);
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: stream,
+    });
+
+    peer.on("signal", (data) => {
+      socket.emit("answerCall", { signal: data, to: caller });
+    });
+
+    peer.on("stream", (stream) => {
+      peerVid.current.srcObject = stream;
+    });
+
+    peer.signal(callerSignal);
+    callRef.current = peer;
+  };
+
+  const endCall = () => {
+    setCallEnded(true);
+    callRef.current.destroy();
+  };
 
   //event handlers
   const compileHandler = async (e) => {
@@ -21,7 +113,10 @@ const Editor = () => {
     };
 
     try {
-      const resp = await axios.post("https://awancors.herokuapp.com/https://api.jdoodle.com/v1/execute", code);
+      const resp = await axios.post(
+        "https://awancors.herokuapp.com/https://api.jdoodle.com/v1/execute",
+        code
+      );
       console.log(resp.data.output);
       setData(resp.data.output);
     } catch (err) {
@@ -76,8 +171,14 @@ const Editor = () => {
       <div className="top-section">
         <div id="firepad-container"></div>
         <div className="video-chat">
-          <div className="vid"></div>
-          <div className="vid"></div>
+          <div className="vid">
+            {stream && <video playsInline muted ref={myVid} autoPlay />}
+          </div>
+          <div className="vid">
+            {callAccepted && !callEnded ? (
+              <video playsInline ref={peerVid} autoPlay />
+            ) : null}
+          </div>
         </div>
       </div>
       <div className="middle-section">
