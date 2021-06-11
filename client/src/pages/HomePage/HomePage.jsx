19c6428ea90d6f1092360eff0c51ffd00272ba3f
Khairo Khatib <khairo.kh@outlook.com>
2021-06-11 11:40:00 -0400

    added some placeholder questions

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -14,10 +14,31 @@ function HomePage() {
             <PostQuestion/>
             </div>
             <QuestionCard 
-            username='sdfgh' 
-            question="Why is react cool?" 
-            questionBrief="fhow i this helping is it great acanvjczhhhhhkrdfhshggfjn"
+            username='Kadd' 
+            question="Can you help me build my project to get it ready for production?" 
+            questionBrief="So I have just finished creating an awesome react project but I'm not sure how to deploy it"
             />
+            <QuestionCard 
+            username='John' 
+            question="What is asynchronous code" 
+            questionBrief="people keep talking about it but I'm not sure exactly what it is"
+            />
+            <QuestionCard 
+            username='Kera' 
+            question="What is a promise? and how to use it?" 
+            questionBrief="I need help with JS promises"
+            />
+            <QuestionCard 
+            username='Jason' 
+            question="Any library to use a rich text editor in react?" 
+            questionBrief="I need a text editor that provides WYSIWYG functionality"
+            />
+            <QuestionCard 
+            username='Tim' 
+            question="Can anyone help me reverse a linkedList" 
+            questionBrief="This is to hard!"
+            />
+          
         </div>
     )
 }
